b2217ec71bbe886d92375844e0a984e93639bd67
Subramaniam Swamynathan <ramani1993@gmail.com>
2016-01-31 09:33:27 -0500

    Create routes.php

---
+++ Programming Assignment 1/routes.php
@@ -0,0 +1,19 @@
+<?php
+
+/*pplication Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It's a breeze. Simply tell Laravel the URIs it should respond to
+| and give it the controller to call when that URI is requested.
+|
+*/
+
+Route::get('/', function(){return 'Hello from Subramaniam Swamynathan;});
+
+Route::get('home', 'HomeController@index');
+
+Route::controllers([
+        'auth' => 'Auth\AuthController',
+        'password' => 'Auth\PasswordController',
+]);
