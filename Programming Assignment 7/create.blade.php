d72d7b78f815f778718f438e9b771a02bef41fa0
Subbu <ramani1993@gmail.com>
2016-04-04 15:25:56 +0000

    changed the crate template

--- Programming Assignment 7/create.blade.php
+++ Programming Assignment 7/create.blade.php
@@ -1,24 +1,41 @@
 <html>
 <head>
 	<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet"/>
+'	<style>
+	  body 
+	  {
+		background-color: lightblue;
+	  }
+	</style>
 </head>
 <body>
 	<form action='/story/store' method="post">
-	Title:<br/>
-	<input  type="text" name="title"/>
-	Location: <br/>
+	<div class="form-group">
+	<label> Title: </label>
+	<input  type="text" class = "form-control" name="title"/>
+	</div>
+	<br/> <br/>
+	<div class="form-group"> 
+	<label> Location: </label>
 	<select name="location_id">
 	@foreach ($locations as $location)
 		<option value="{{ $location->id }}">{{$location->value }}</option>
 	@endforeach
 	</select>
-	Story:<br/>
-	<input type="text" name="story"/>
+	</div>
+	<br/> <br/> 
+	<div class="form-group">
+	<label> Story: </label>
+	<input class = "form-control" type="text" name="story"/>
+	</div>
+	<div class="form-group">
+	<label> Published: </label>
 	<select name="published">
-		<option value="1">True</option>
+w		<option value="1">True</option>
 		<option value="0">False</option>
 	</select>
-	<input type="submit" name="Submit"/>
+	</div>
+	<input type="submit" name="Submit" class="btn btn-success"/> </center>
 	</form>
 </body>
 </html>
